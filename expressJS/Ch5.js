f265cf4628c8ef7ec57563fe2ded800224d6ad2e
Devendra Singh Baghel <imdbsingh4u@gmail.com>
2025-11-24 20:48:31 +0530

    Add new chapter

--- expressJS/Ch5.js
+++ expressJS/Ch5.js
@@ -4,3 +4,32 @@ Built-in Middleware in express js
 Q1. What is Built-in Middleware.
 Q2. Examples of 2 Built-in Middleware.
 */
+
+import express from "express";
+import path from "path";
+
+const app = express();
+
+const PORT = 8000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  const absPath = path.resolve("files/login.html");
+  res.sendFile(absPath);
+});
+
+app.get("/signup", (req, res) => {
+  const absPath = path.resolve("files/signup.html");
+  res.sendFile(absPath);
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  res.send("<h1>Form Submited.</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log(`App is listening on PORT ${PORT}`);
+});
