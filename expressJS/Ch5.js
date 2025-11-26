e64d3e8058e25c4a70fc22d7ad2e31f73606be08
Devendra Singh Baghel <imdbsingh4u@gmail.com>
2025-11-26 20:27:10 +0530

    Add new chapter

--- expressJS/Ch5.js
+++ expressJS/Ch5.js
@@ -12,6 +12,7 @@ const app = express();
 
 const PORT = 8000;
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
--- expressJS/Ch7.js
+++ expressJS/Ch7.js
@@ -4,3 +4,31 @@ Error-Handling Middleware
 Q1. What is Error-Handling Middleware.
 Q2. Example of Error-Handling middleware.
 */
+
+import express from "express";
+
+const app = express();
+
+const PORT = 8000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Home Page !</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send1("<h1>About Page !</h1>");
+});
+
+app.get("/error", (req, res) => {
+  res.send("<h1>Error Page !</h1>");
+});
+
+app.use((error, req, res, next) => {
+  res
+    .status(error.status || 500)
+    .send("Something went wrong, try after some time!");
+});
+
+app.listen(PORT, () => {
+  console.log(`App is listening on PORT ${PORT}`);
+});
